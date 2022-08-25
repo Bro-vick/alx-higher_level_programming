be35ccfaed7435945d9df44fb4c5ea378b149dfc
George victor <georgev421@gmail.com>
2022-08-25 16:04:34 -0700

    hidden secret

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,7 +2,6 @@
 
 import hidden_4
 if __name__ == "__main__":
-
-for content in dir(hidden_4):
-	if content[:2] != "__":
-		print("{:s}".format(content))
+    for content in dir(hidden_4):
+        if content[:2] != "__":
+            print("{:s}".format(content))
