8b27187ca4174762b32b1a6c47207a6a9daa0ecd
george <georgev421@gmail.com>
2023-01-17 20:25:56 +0100

    task 5 refactoring

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -2,18 +2,15 @@
 // script gets the content of a webpage
 // stores it in a file
 
-const fs = require('fs');
 const request = require('request');
+const fs = require('fs');
+const endpoint = process.argv[2];
+const file = process.argv[3];
 
-const url = process.argv[2];
-const filename = process.argv[3];
+request(endpoint, (err, res, body) => {
+  if (err) throw err;
 
-request.get(url, function (error, response, body) {
-	if (!error && response.statusCode === 200) {
-		fs.writeFile(filename, body, function (err) {
-			if (err) console.error(err);
-		});
-	} else {
-		console.error(error);
-	}
+  fs.writeFile(file, body, 'utf8', (err) => {
+    if (err) throw err;
+  });
 });
