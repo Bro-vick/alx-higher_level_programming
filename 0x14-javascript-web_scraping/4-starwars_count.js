440ba0838dc8d5fafa2cd805de50ef63144d9132
george <georgev421@gmail.com>
2023-01-17 20:27:57 +0100

    task 4 refactoring

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,17 +2,17 @@
 // script prints the number of movies where
 // the character "wedge Antilles" is present
 
-const req = require('request');
+const request = require('request');
 const endpoint = process.argv[2];
 let count = 0;
 
-req(endpoint, (err, res, body) => {
-	if (err) throw err;
+request(endpoint, (err, res, body) => {
+  if (err) throw err;
 
-	JSON.parse(body).results.forEach(result => {
-		result.characters.forEach(character => {
-			if (character.endsWith('18/')) count++;
-		});
-	});
-	console.log(count);
+  JSON.parse(body).results.forEach(result => {
+    result.characters.forEach(character => {
+      if (character.endsWith('18/')) count++;
+    });
+  });
+  console.log(count);
 });
