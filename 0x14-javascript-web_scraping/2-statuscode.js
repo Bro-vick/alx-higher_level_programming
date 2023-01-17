77894a4db85024bed17e276e779b0892288a2000
george <georgev421@gmail.com>
2023-01-17 20:34:31 +0100

    task 2 refactoring

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,12 +1,9 @@
 #!/usr/bin/node
 // script displays the status code of a Get request
 
-const req = require('request');
-const url = process.argv[2];
+const request = require('request');
+const endpoint = process.argv[2];
 
-req.get(url, function (err, response, body) {
-  if (err) console.error(err); else {
-     const code = response.statusCode;
-     console.log(`code: ${code}`);
-  }
+request(endpoint, (err, res, body) => {
+  if (!err) console.log('code:', res.statusCode);
 });
